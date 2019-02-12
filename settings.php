03de9bb91802e9785fef69c47a6651006eb4da5b
Tsarev Vladimir <sau412@gmail.com>
2019-02-12 15:15:43 +0300

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,30 @@
+<?php
+// Settings file
+
+$project_name="Microgrid";
+$currency_short="GRC";
+$currency="Gridcoin";
+
+// DB variables
+$db_host="";
+$db_login="";
+$db_password="";
+$db_base="";
+
+// Salt for password
+$salt="";
+
+// Site settings
+$settings_payouts_enabled=TRUE;
+$settings_login_enabled=TRUE;
+
+// Gridcoin URLs
+$address_url="https://www.gridcoinstats.eu/address/";
+$tx_url="https://www.gridcoinstats.eu/tx/";
+$block_url="https://www.gridcoinstats.eu/block/";
+
+// Gridcoin online wallet API key
+$grc_api_url="https://wallet.arikado.ru/api.php";
+$grc_api_key="";
+
+?>
