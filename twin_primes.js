9d5edcd0c9032adfaa06bb90997c5db75e31d029
scribblemaniac <scribblemaniac@users.noreply.github.com>
2019-03-08 01:53:58 -0700

    Simplify and optimize the twin prime project further
    Based mostly off of the property of twin primes being of the form
    (6n-1,6n+1), as well as the properties of n.

--- twin_primes.js
+++ twin_primes.js
@@ -9,36 +9,29 @@ self.addEventListener('message', function(e) {
 
         // Check each number and report progress
         function check_number_seq(thread_index, start_number, stop_number) {
-                var number;
+                var number, n, nMod;
                 var seq_result = [];
                 var is_prime;
                 var progress = 0;
-                progress_report_interval=Math.floor((stop_number-start_number)/100);
+                var start_n = Math.ceil(start_number / 6);
+                var number_dif = stop_number-start_number;
+                progress_report_interval=Math.floor(number_dif/100);
                 progress_report=start_number;
 
-                if((start_number % 2) == 0) start_number++;
+                // All twin primes except (3,5) are of the form (6n-1,6n+1), so we
+                // iterate over n, with number = 6n and checking number-1 and number+1
+                for(n=start_n,number=start_n*6; number<=stop_number; n++,number+=6) {
+                        // All n such that 6n-1 and 6n+1 are prime must have the units digit 0, 2, 3, 5, 7, or 8
+                        nMod = n % 5;
+                        if(nMod == 1 || nMod == 4) continue;
 
-                for(number=start_number; number<=stop_number; number+=4) {
-                        // Check number
-                        if(check_is_prime(number)) {
-                          if(check_is_prime(number+2)) {
-                            // Add number to results
-                            seq_result.push(number);
-                            number+=4;
-                          }
-                          else {
-                            // Optimization - number+2 is false so we can skip it
-                            number+=2;
-                          }
-                          if(check_is_prime(number-2)) {
-                            // Add number to results
-                            seq_result.push(number-2);
-                            number+=2;
-                          }
+                        // Check (6n-1,6n+1)
+                        if(check_is_prime(number-1) && check_is_prime(number+1)) {
+                            seq_result.push(number-1);
                         }
                         // Report progress
                         if(number > progress_report) {
-                                progress=(number-start_number)/(stop_number-start_number);
+                                progress=(number-start_number)/number_dif;
                                 self.postMessage([thread_index,0,progress]);
                                 progress_report+=progress_report_interval;
                         }
