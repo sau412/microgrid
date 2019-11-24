896f0e3572f69a7091b1c17dc0b351026ffde941
User <sau412@protonmail.com>
2019-11-24 12:21:20 +0300

    Link fix

--- lib/html.php
+++ lib/html.php
@@ -31,7 +31,7 @@ function html_page_end() {
 
 	$result=<<<_END
 <hr width=10%>
-<p>Opensource Gridcoin distribute computing project (<a href='https://github.com/sau412/microgrid'>github</a>) by <a href='htps://arikado.xyz/'>sau412</a>.</p>
+<p>Opensource Gridcoin distribute computing project (<a href='https://github.com/sau412/microgrid'>github</a>) by <a href='https://arikado.xyz/'>sau412</a>.</p>
 <p><img src='https://arikado.xyz/counter/?site=$project_counter_name'></p>
 </center>
 <script>
