fb737ce98a9b2cc8014e639e649ad404c75c05ad
sau412 <sau412@protonmail.com>
2020-05-19 10:20:18 +0300

    Stats from DB

--- web/cacti_stats.php
+++ web/cacti_stats.php
@@ -1,24 +1,18 @@
 <?php
 require_once("../lib/settings.php");
 require_once("../lib/db.php");
+require_once("../lib/core.php");
 
 db_connect();
 
-$users=db_query_to_variable("SELECT count(*) FROM `users`");
-
-$active_users=db_query_to_variable("SELECT count(DISTINCT `user_uid`) FROM `workunit_results` WHERE DATE_SUB(NOW(),INTERVAL 1 HOUR)<`created`");
-
-$workunits=db_query_to_variable("SELECT count(*) FROM `workunits`");
-
-$results=db_query_to_variable("SELECT count(*) FROM `workunit_results`");
-
-$workunits_complete=db_query_to_variable("SELECT count(*) FROM `workunits` WHERE `is_completed`=1");
-
-$results_complete=db_query_to_variable("SELECT count(*) FROM `workunit_results` WHERE `result_hash` IS NOT NULL");
-
-$wallet_balance=db_query_to_variable("SELECT `value` FROM `variables` WHERE `name`='wallet_balance'");
-
-$users_balance=db_query_to_variable("SELECT SUM(`balance`) FROM `users`");
+$users = get_variable('users');
+$active_users = get_variable('active_users');
+$workunits = get_variable('workunits');
+$results = get_variable('results');
+$workunits_complete = get_variable('workunits_complete');
+$results_complete = get_variable('results_complete');
+$wallet_balance = get_variable('wallet_balance');
+$users_balance = get_variable('users_balance');
 
 echo "users:$users active_users:$active_users workunits:$workunits workunits_complete:$workunits_complete results:$results results_complete:$results_complete wallet_balance:$wallet_balance users_balance:$users_balance\n";
 ?>
