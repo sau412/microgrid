abd19b5dc541c68c8d1d2cd313b0abcbff42703e
sau412 <sau412@protonmail.com>
2020-08-04 08:56:57 +0300

    Start button enabled

--- lib/html.php
+++ lib/html.php
@@ -411,6 +411,7 @@ if(typeof(navigator.hardwareConcurrency) !== 'undefined') {
 function stop_on_completed() {
 	auto_load_next_enabled = 0;
 	document.getElementById('pause_button').disabled=true;
+	document.getElementById('start_button').disabled=false;
 }
 
 function start_calculations() {
