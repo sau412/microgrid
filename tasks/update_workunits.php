6428a76f8a3321f7516ca3598a3250dc5766ef94
sau412 <sau412@protonmail.com>
2020-08-25 20:32:18 +0300

    Fix old workunits

--- tasks/update_workunits.php
+++ tasks/update_workunits.php
@@ -9,7 +9,24 @@ require_once("../lib/core.php");
 
 db_connect();
 
-$old_workunits_array=db_query_to_array("SELECT `uid`,`workunit_uid` FROM `workunit_results` WHERE `result_hash` IS NULL AND DATE_SUB(NOW(),INTERVAL $workunit_max_interval)>`created`");
+$workunits_in_progress_array = db_query_to_array("SELECT `uid` FROM `workunits` WHERE 'in_progress' = 1");
+
+foreach($workunits_in_progress_array as $row) {
+	$workunit_uid = $row['uid'];
+	$workunit_uid_escaped = db_escape($workunit_uid);
+
+	$is_old = db_query_to_variable("SELECT 1 FROM `workunit_results`
+										WHERE `result_hash` IS NULL AND
+											DATE_SUB(NOW(),INTERVAL $workunit_max_interval) < `created` AND
+											`workunit_uid` = '$workunit_uid_escaped'");
+	if($is_old) {
+		echo "workunit_uid $workunit_uid\n";
+	}
+}
+die();
+$old_workunits_array=db_query_to_array("SELECT `uid`, `workunit_uid`
+		FROM `workunit_results`
+		WHERE `result_hash` IS NULL AND DATE_SUB(NOW(),INTERVAL $workunit_max_interval)>`created`");
 
 foreach($old_workunits_array as $row) {
 	$uid=$row['uid'];
