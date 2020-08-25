5c36b3e63d6b0e7ad1a1ffd47f3dedecfa32f6f3
sau412 <sau412@protonmail.com>
2020-08-25 18:26:05 +0300

    Query fix

--- tasks/dump_remove_results.php
+++ tasks/dump_remove_results.php
@@ -4,13 +4,13 @@ require_once("../lib/db.php");
 
 db_connect();
 
-$projects_array = db_query("SELECT `uid` FROM `projects`");
+$projects_array = db_query_to_array("SELECT `uid` FROM `projects`");
 
 foreach($projects_array as $project) {
     $project_uid = $project['uid'];
     $project_uid_escaped = db_escape($project_uid);
 
-    $result=db_query("SELECT `uid`, `start_number`, `stop_number`, `result`, `is_completed` FROM `workunits`
+    $result = db_query("SELECT `uid`, `start_number`, `stop_number`, `result`, `is_completed` FROM `workunits`
                         WHERE `project_uid`='$project_uid_escaped' AND DATE_SUB(NOW(), INTERVAL 1 MONTH) > `timestamp`
                         ORDER BY `start_number` LIMIT 100");
     while($row = mysql_fetch_assoc($result)) {
